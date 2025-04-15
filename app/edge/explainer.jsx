059b136543174edb50475ea24b97f79801c4a325
coolkarnirk <45289462+coolkarnirk@users.noreply.github.com>
2025-04-15 23:25:36 +0530

    comment unwanted imports

--- app/edge/explainer.jsx
+++ app/edge/explainer.jsx
@@ -1,24 +1,24 @@
-import { Markdown } from 'components/markdown';
+// import { Markdown } from 'components/markdown';
 
-const explainer = `
-This page is using a Netlify Edge Function (\`netlify/edge-functions/rewrite.js\`) to rewrite the URL based on visitor geography.
+// const explainer = `
+// This page is using a Netlify Edge Function (\`netlify/edge-functions/rewrite.js\`) to rewrite the URL based on visitor geography.
 
-~~~js
-const rewrite = async (request, context) => {
-    const path = context.geo?.country?.code === 'AU' ? '/edge/australia' : '/edge/not-australia';
-    return new URL(path, request.url);
-};
+// ~~~js
+// const rewrite = async (request, context) => {
+//     const path = context.geo?.country?.code === 'AU' ? '/edge/australia' : '/edge/not-australia';
+//     return new URL(path, request.url);
+// };
 
-export const config = {
-    path: '/edge'
-};
+// export const config = {
+//     path: '/edge'
+// };
 
-export default rewrite;
-~~~
+// export default rewrite;
+// ~~~
 
-[See more examples](https://edge-functions-examples.netlify.app)
-`;
+// [See more examples](https://edge-functions-examples.netlify.app)
+// `;
 
-export default function EdgeFunctionExplainer() {
-    return <Markdown content={explainer} />;
-}
+// export default function EdgeFunctionExplainer() {
+//     return <Markdown content={explainer} />;
+// }
